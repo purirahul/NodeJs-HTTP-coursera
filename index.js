4fc4c45b34355e86bd7d06e60882676e10d38e30
purirahul <rpuri648@gmail.com>
2020-09-16 22:08:57 +0530

    More configuration  in HTTP  server

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = "localhost";
 const port = "3000";
 
 const server = http.createServer((req,res) => {
-  console.log(req.headers);
+  console.log('Request for ' + req.url + ' by method ' + req.method);
 
-  req.statusCode = 200;
-  res.setHeader('content-type', 'text/html');
-  res.end("<html><body><h1>Hell0 World</body></html>");
+  if(req.method == 'GET'){
+
+        var fileUrl;
+        if (req.url == '/') fileUrl = '/index.html';
+        else fileUrl = req.url;
+
+        var filePath = path.resolve('./public' + fileUrl);
+        var fileExt = path.extname(filePath);
+        console.log(fileExt);
+        if (fileExt == '.html'){
+            fs.exists(filePath, (exists) => {
+              if(!exists){
+                res.statusCode = 404;
+                res.setHeader('Content-type', 'text/html');
+                res.end('<html><body><h1>Error 404 :' + fileUrl + 'not found</h1></body></html>');
+              }
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'text/html');
+              fs.createReadStream(filePath).pipe(res);
+            });
+        }
+
+      else{
+        res.statusCode = 404;
+        res.setHeader('Content-type', 'text/html');
+        res.end('<html><body><h1>Error 404 :' + fileUrl + 'not a html file</h1></body></html>');
+      }
+
+    }
+
+  else{
+    res.statusCode = 404;
+    res.setHeader('Content-type', 'text/html');
+    res.end('<html><body><h1>Error 404 :' + req.method + 'not supported</h1></body></html>');
+  }
+
+
+
 });
 
 server.listen(port, hostname, () => {
